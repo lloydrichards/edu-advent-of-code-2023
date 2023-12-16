b4eff91f5b95ac33e290f55c8e664bb9f5e46304
lloydrichards <lloyd.d.richards@gmail.com>
2023-12-16 19:21:28 +0100

    docs(advent): :memo: expand template

--- src/_template/day_test.ts
+++ src/_template/day_test.ts
@@ -1,5 +1,10 @@
 // import { describe, expect, test } from "bun:test";
+// import { main as partOne } from "./part_1";
 
-// describe.skip("AoC: Day #", () => {
-//   test("part 1", async () => {});
+// import example from "./input/example.txt";
+
+// describe("AoC: Day #", () => {
+//   test("part 1", async () => {
+//     expect(partOne(example)).toEqual(0);
+//   });
 // });
